7797b8a4e6ab2196c6807da552d24264c8eca0dd
Jpdsf <jpdsafg@gmail.com>
2023-05-16 16:21:02 -0300

    Ajeitando dnv o html sobre nos

---
+++ sobre-nos.js
@@ -0,0 +1,37 @@
+
+let infDesk = document.getElementById('informacoes-pc')
+
+if(WURFL.form_factor === 'Desktop'){
+    console.log('pc')
+    infDesk.innerHTML = `
+    <div class="informacoes-config text-center">
+    <h1> THE NO MAPA </h1>
+    <div class="sobre">
+        <div class="texto-sobre">
+            É um projeto que tem o objetivo de conhecer e divulgar iniciativas sociais que contribuem para o desenvolvimento de toda a população local, para que assim a comunidade tenha a oportunidade de se inserir nessas iniciativas de acordo com seus interesses
+        </div>
+        <div class="img-sobre">
+            <img src="img/logo_projeto_transparente.png" alt="" srcset="">
+            
+        </div>
+    </div>
+</div>`
+} else {
+    console.log('nao-pc')
+    infDesk.innerHTML = `
+    <div class="informacoes-config text-center">
+    <h1> THE NO MAPA </h1>
+    <div class="">
+        <div class="texto-sobre">
+            É um projeto que tem o objetivo de conhecer e divulgar iniciativas sociais que contribuem para o desenvolvimento de toda a população local, para que assim a comunidade tenha a oportunidade de se inserir nessas iniciativas de acordo com seus interesses
+        </div>
+        <div class="img-sobre">
+            <img src="img/logo_projeto_transparente.png" alt="" srcset="">
+            
+        </div>
+    </div>
+</div>`
+
+
+
+}
