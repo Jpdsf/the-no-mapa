b5df48594d757368aee9c54688c03a0bc7936a3b
Jpdsf <jpdsafg@gmail.com>
2023-05-24 01:43:08 -0300

    Refatorando o codigo parte2

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let map;
 let mapa = document.getElementById('mapa')
-let interacao = document.getElementById('interacao')
-let interacaoPc = document.getElementById('interacao-pc')
-let fechar = document.getElementById('fechar')
 
 let pessoa = {
   nomeProjeto: '',
@@ -49,21 +46,20 @@ aJMTObj.informacao1 = '<div><a class="cadastro redes-contato" href="https://inst
 aJMTObj.informacao2 = '<div><a class="cadastro redes-contato" href=""><img src="https://cdn-icons-png.flaticon.com/512/281/281769.png" alt="" srcset=""> Gmail: ajmt2021associacao@gmail.com</a></div>'
 aJMTObj.informacao3 = '<div><a class="cadastro redes-contato" href="https://l.wl.co/l?u=https%3A%2F%2Fforms.gle%2FqS1WgG68sK4cBZCF6"><img src="https://cdn-icons-png.flaticon.com/512/2875/2875409.png">Formulário de inscrição </a></div>'
 
-
-//Função para aparecer informações da ong no Pc
-function informacoes(pessoa){
-  interacaoPc.innerHTML = `
+function informacoes(pessoa) {
+  interacao.innerHTML = `
   <div class="interacao-config">
-    <label for="floatingInput">NOME:</label>${pessoa.nomeProjeto}
-  <br>
-  <label for="floatingInput">O QUE ELES FAZEM:</label> ${pessoa.objProjeto}
-  <label for="floatingInput">LOCALIZAÇÃO:</label> ${pessoa.localizacaoProjeto}
-  <br>
-  <br>
-  <div class="text-center" >
+      <label for="floatingInput">NOME:</label>${pessoa.nomeProjeto}
+      <br>
+      <label for="floatingInput">O QUE ELES FAZEM:</label> ${pessoa.objProjeto}
+      <br>
+      <label for="floatingInput">LOCALIZAÇÃO:</label> ${pessoa.localizacaoProjeto}
+      <br>
+      <br>
+    <div class="text-center" >
     <label for="floatingInput" style="color: white; margin-bottom: 10px" >ENTRE EM CONTATO:</label>
     <br>
-    <div>
+      <div>
       ${pessoa.informacao1}
       ${pessoa.informacao2}
       ${pessoa.informacao3}
@@ -75,173 +71,108 @@ function informacoes(pessoa){
   `
 }
 
-//Função para aparecer informações da ong em smartfone
-function informacoesCell(pessoa){
-  interacao.innerHTML = `
-  <div class="interacao-config">
-    <label for="floatingInput">NOME:</label>${pessoa.nomeProjeto}
-  <br>
-  <label for="floatingInput">O QUE ELES FAZEM:</label> ${pessoa.objProjeto}
-  <label for="floatingInput">LOCALIZAÇÃO:</label> ${pessoa.localizacaoProjeto}
-  <br>
-  <br>
-  <div class="text-center" >
-    <label for="floatingInput" style="color: white; margin-bottom: 10px" >ENTRE EM CONTATO:</label>
-    <br>
-    <div>
-      ${pessoa.informacao1}
-      ${pessoa.informacao2}
-      ${pessoa.informacao3}
-      ${pessoa.informacao4}
-      ${pessoa.informacao5}
-      </div>
-  </div>
-</div>
-  `
+function mapFuction() {
+  function sucess(pos) {
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 15);
+    L.tileLayer('https://api.mapbox.com/styles/v1/jpdsafg/clhm3jnhq00qs01qsawcjcd7m/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianBkc2FmZyIsImEiOiJjbGdjeWhhYjEwMzlqM2VybmlhemJiczB4In0.XmLwqlWabpGKax8ZrFqhqA', {
+    }).addTo(map);
+    let violaoIcon = L.icon({
+      iconUrl: 'img/violao-icon-sem-fundo.png',
+      iconSize: [40, 55]
+    });
+
+    let pessoaIcon = L.icon({
+      iconUrl: 'img/icon-alfinete.png',
+      iconSize: [45, 55]
+
+    });
+
+    let localizacaoPessoa = L.marker([pos.coords.latitude, pos.coords.longitude], { icon: pessoaIcon }).addTo(map)
+      .bindPopup('Sua localização')
+      .openPopup();
+
+    let musicaParaTodos = L.marker([-5.080293661150606, -42.77629429380992], { icon: violaoIcon }).addTo(map)
+      .bindPopup('MÚSICA PARA TODOS');
+
+    let palacioDaMusica = L.marker([-5.097176221125296, -42.81075604962941], { icon: violaoIcon }).addTo(map)
+      .bindPopup('PALÁCIO DA MÚSICA');
+
+    let escolaMusicaDonaGal = L.marker([-5.066833962379359, -42.82844716931162], { icon: violaoIcon }).addTo(map)
+      .bindPopup('ESCOLA de música DONA GAL');
+
+    let orgPontoDeEquilibrio = L.marker([-5.095850538207881, -42.81286937116396]).addTo(map)
+      .bindPopup('Org Ponto De Equilibrio');
+
+    let aJMT = L.marker([-5.141800, -42.788900]).addTo(map)
+      .bindPopup('Associação de juventude AJMT');
+
+    musicaParaTodos.on('click', function () {
+      informacoes(musicaParaTodosObj);
+    })
+
+    palacioDaMusica.on('click', function () {
+      informacoes(palacioDaMusicaObj);
+    })
+
+    escolaMusicaDonaGal.on('click', function () {
+      informacoes(escolaMusicaDonaGalObj);
+    })
+
+    orgPontoDeEquilibrio.on('click', function () {
+      informacoes(orgPontoDeEquilibrioObj);
+    })
+
+    aJMT.on('click', function () {
+      informacoes(aJMTObj);
+    })
+
+    localizacaoPessoa.on('click', function () {
+      interacao.innerHTML = ``
+    });
+
+  }
+  function error(err) {
+    console.log(err)
+  }
+  let wathID = navigator.geolocation.watchPosition(sucess, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+  });
 }
 
 if (WURFL.form_factor === 'Desktop') {
+  interacao = document.getElementById('interacao-pc')
+  let styleElement = document.createElement('style');
 
-    function sucess(pos) {
-        console.log(pos.coords.latitude, pos.coords.longitude);
-        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 15);
-        L.tileLayer('https://api.mapbox.com/styles/v1/jpdsafg/clhm3jnhq00qs01qsawcjcd7m/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianBkc2FmZyIsImEiOiJjbGdjeWhhYjEwMzlqM2VybmlhemJiczB4In0.XmLwqlWabpGKax8ZrFqhqA', {
-        }).addTo(map);
-        let violaoIcon = L.icon({
-            iconUrl: 'img/violao-icon-sem-fundo.png',
-            iconSize: [40, 55]
-        });
+  let styles = `
+  .interacao-pc{
+    font-size: 20px;
+    margin-left: 10px;
+    max-width: 430px;
+    text-align: justify;
+  }`
 
-        let pessoaIcon = L.icon({
-            iconUrl: 'img/icon-alfinete.png',
-            iconSize: [45, 55]
+  styleElement.innerHTML = styles;
+  let headElement = document.head || document.getElementsByTagName('head')[0];
+  headElement.appendChild(styleElement);
 
-        });
-
-        let localizacaoPessoa = L.marker([pos.coords.latitude, pos.coords.longitude], { icon: pessoaIcon }).addTo(map)
-            .bindPopup('Sua localização')
-            .openPopup();
-
-        let musicaParaTodos = L.marker([-5.080293661150606, -42.77629429380992], { icon: violaoIcon }).addTo(map)
-        .bindPopup('MÚSICA PARA TODOS');
-
-        let palacioDaMusica = L.marker([-5.097176221125296, -42.81075604962941], { icon: violaoIcon }).addTo(map)
-        .bindPopup('PALÁCIO DA MÚSICA');
-
-        let escolaMusicaDonaGal = L.marker([-5.066833962379359, -42.82844716931162], { icon: violaoIcon }).addTo(map)
-        .bindPopup('ESCOLA de música DONA GAL');
-
-        let orgPontoDeEquilibrio = L.marker([-5.095850538207881, -42.81286937116396]).addTo(map)
-        .bindPopup('Org Ponto De Equilibrio');
-
-        let aJMT = L.marker([-5.141800,-42.788900]).addTo(map)
-        .bindPopup('Associação de juventude AJMT');
-
-        musicaParaTodos.on('click', function(){
-          informacoes(musicaParaTodosObj);
-        })
-
-        palacioDaMusica.on('click', function(){
-          informacoes(palacioDaMusicaObj);
-        })
-
-        escolaMusicaDonaGal.on('click', function(){
-          informacoes(escolaMusicaDonaGalObj);
-        })
-
-        orgPontoDeEquilibrio.on('click', function(){
-          informacoes(orgPontoDeEquilibrioObj);
-        })
-
-        aJMT.on('click', function(){
-          informacoes(aJMTObj);  
-        })
-
-        localizacaoPessoa.on('click', function () {
-            interacaoPc.innerHTML = ``
-        });
-
-    }
-
-    function error(err) {
-        console.log(err)
-    }
-
-    let wathID = navigator.geolocation.watchPosition(sucess, error, {
-        enableHighAccuracy: true,
-        timeout: 5000
-    });
-
+  mapFuction()
 
 } else {
-    console.log('mobile')
-    function sucess(pos) {
-        console.log(pos.coords.latitude, pos.coords.longitude);
+  console.log('mobile')
+  let interacao = document.getElementById('interacao')
+  let styleElement = document.createElement('style');
 
-        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 15);
-        
-        L.tileLayer('https://api.mapbox.com/styles/v1/jpdsafg/clhm3jnhq00qs01qsawcjcd7m/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianBkc2FmZyIsImEiOiJjbGdjeWhhYjEwMzlqM2VybmlhemJiczB4In0.XmLwqlWabpGKax8ZrFqhqA', {
-        //L.tileLayer('https://api.mapbox.com/styles/v1/jpdsafg/clgdwcvcq00pz01ppsrbhc75j/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianBkc2FmZyIsImEiOiJjbGdjeWhhYjEwMzlqM2VybmlhemJiczB4In0.XmLwqlWabpGKax8ZrFqhqA', {
-        }).addTo(map);
-        let violaoIcon = L.icon({
-            iconUrl: 'img/violao-icon-sem-fundo.png',
-            iconSize: [40, 55]
-        });
+  let styles = `
+  .interacao{
+    font-size: 20px;
+    width: 100%
+  }
+  `
+  styleElement.innerHTML = styles;
+  let bodyElement = document.body || document.getElementsByTagName('body')[0];
+  bodyElement.appendChild(styleElement);
 
-        let pessoaIcon = L.icon({
-          iconUrl: 'img/icon-alfinete.png',
-          iconSize: [45, 55] 
-        });   
-
-        let localizacaoPessoa = L.marker([pos.coords.latitude, pos.coords.longitude], { icon: pessoaIcon }).addTo(map)
-            .bindPopup('Sua localização')
-            .openPopup();
-
-        let musicaParaTodos = L.marker([-5.080293661150606, -42.77629429380992], { icon: violaoIcon }).addTo(map)
-        .bindPopup('MÚSICA PARA TODOS');
-
-        let palacioDaMusica = L.marker([-5.097176221125296, -42.81075604962941], { icon: violaoIcon }).addTo(map)
-        .bindPopup('PALÁCIO DA MÚSICA');
-
-        let escolaMusicaDonaGal = L.marker([-5.066833962379359, -42.82844716931162], { icon: violaoIcon }).addTo(map)
-        .bindPopup('ESCOLA de música DONA GAL');
-
-        let orgPontoDeEquilibrio = L.marker([-5.095850538207881, -42.81286937116396]).addTo(map)
-        .bindPopup('Org Ponto De Equilibrio');
-
-        let aJMT = L.marker([-5.141800,-42.788900]).addTo(map)
-        .bindPopup('Org Ponto De Equilibrio');
-
-        musicaParaTodos.on('click', function(){
-          informacoesCell(musicaParaTodosObj);
-        })
-
-        palacioDaMusica.on('click', function(){
-          informacoesCell(palacioDaMusicaObj);
-        })
-
-        escolaMusicaDonaGal.on('click', function(){
-          informacoesCell(escolaMusicaDonaGalObj);
-        })
-
-        orgPontoDeEquilibrio.on('click', function(){
-          informacoesCell(orgPontoDeEquilibrioObj);
-        })
-
-        aJMT.on('click', function(){
-          informacoesCell(aJMTObj);  
-        })
-
-        localizacaoPessoa.on('click', function () {
-            interacao.innerHTML = ``
-        });
-    }
-    function error(err) {
-        console.log(err)
-    }
-    let wathID = navigator.geolocation.watchPosition(sucess, error, {
-        enableHighAccuracy: true,
-        timeout: 5000
-    });
-
+  mapFuction()
 }
